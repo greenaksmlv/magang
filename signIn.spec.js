32b2fa0e2a8ecbeba59e2b3297fb59507f686663
greenmelissa <2281065@unai.edu>
2025-07-01 14:13:39 +0700

    Menambahkan dokumentasi untuk signIn.spec.js

--- signIn.spec.js
+++ signIn.spec.js
@@ -1,6 +1,20 @@
 const { config } = require('./config');
 const { test, expect } = require('./setup');
 
+
+/**
+ * Fungsi untuk melakukan proses Sign In
+ * 
+ * Alur: 
+ * - Cari tombol "Masuk" di halaman utama
+ * - Klik tombol Masuk
+ * - Verifikasi halaman login muncul
+ * - Cari dan klik tombol metode login yang disesuaikan dengan `method`
+ * 
+ * @param {object} webApp - Konteks Playwright browser 
+ * @param {string} method - Key metode login yang disesuaikan dengan konfigurasi di config.sign_methods
+ */
+
 // Helper function to Sign In
 async function signIn(webApp, method) {
     test.info().annotations.push({
@@ -16,6 +30,20 @@ async function signIn(webApp, method) {
     await expect(button).toBeVisible();
     await button.click();
 }
+
+/**
+ * Pengujian utama untuk proses Sign In
+ * 
+ * Tujuan:
+ * - Memastikan bahwa halaman login dapat diakses
+ * - Memastikan bahwa tombol login menggunakan metode tertentu dapat diklik
+ * 
+ * Allure Labels:
+ * - feature: sign in
+ * - severity: normal
+ * - platform: web
+ * - status: pass
+ */
 
 // Main test
 test('sign in', async ({ webApp }) => {
